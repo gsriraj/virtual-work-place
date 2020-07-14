5c1142eea3696ff4a9eaff97079a7714968d1675
Sriraj G <srirajg@deloitte.com>
2020-07-14 19:21:06 +0530

    base layout of me initiated

--- fe/src/pages/main/tabs/me/me.tsx
+++ fe/src/pages/main/tabs/me/me.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Layout, Avatar, Card, Space } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 import './me.css';
 
+const { Header, Content, Sider } = Layout;
 
 function Me() {
     return (
-        <div>
-           Me tab
+        <div className="me-body-spec">
+            <Layout style={{ minHeight: '100vh', marginLeft: '-1rem' }}>
+                <Content>
+                    <Card style={{ width: 300 }}>
+                        <Space size="middle" direction="vertical">
+                            <Avatar size={64} icon={<UserOutlined />} />
+                            <h3>Dummy User</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, laborum perferendis delectus necessitatibus, nesciunt, nam enim in odit quisquam facilis dolore ipsa! Sed quasi dolore eius? Aut minima consequuntur sint.</p>
+                        </Space>
+                    </Card>
+                </Content>
+            </Layout>
         </div>
     );
 }
